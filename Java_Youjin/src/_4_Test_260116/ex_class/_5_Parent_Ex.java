42d82c8874cdf3fbacdde8daf44857acffee615e
chyoujin93 <chyoujin93@gmail.com>
2026-01-16 17:40:39 +0900

    0116최종

--- Java_Youjin/src/_4_Test_260116/ex_class/_5_Parent_Ex.java
+++
@@ -1,9 +0,0 @@
-package _4_Test_260116.ex_class;
-
-public class _5_Parent_Ex {
-    public void hello() {
-        System.out.println("순서3,  부모 super.hello() 호출 중");
-        System.out.println("안녕, 부모입니다.");
-        System.out.println("순서4,  부모 super.hello() 호출 후");
-    }
-}
